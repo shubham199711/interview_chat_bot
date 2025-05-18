fb4ea041d148361ace6cc8ad87efd62b2919f2c7
Shubham Dohare <sdohare11@gmail.com>
2025-05-18 22:01:48 +0530

    fixed all the things

--- backend/app/db.py
+++ backend/app/db.py
@@ -2,8 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# SQLALCHEMY_DATABASE_URL = "postgresql://user:password@db:5432/interviewdb"
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:postgres@localhost:5432/interviewdb"
+SQLALCHEMY_DATABASE_URL = "postgresql://user:password@db:5432/interviewdb"
+# SQLALCHEMY_DATABASE_URL = "postgresql://postgres:postgres@localhost:5432/interviewdb"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
